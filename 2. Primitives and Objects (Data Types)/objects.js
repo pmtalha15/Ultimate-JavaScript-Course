82eab97cbe2fdb3047711e6c66d524b426dd1498
Talha <p.m.talha15@gmail.com>
2023-05-14 11:39:28 +0530

    Understanding datatypes and objects in depth

---
+++ 2. Primitives and Objects (Data Types)/objects.js
@@ -0,0 +1,10 @@
+// Objects are key value pairs
+
+const item = {
+    "Talha": true,
+    "Zaid": false,
+    "Ismail": 12
+}
+
+console.log(item["Zaid"]); // Will print the value false
+console.log(item["SDF"]); // Will print undefined as it is not available in the item object.
