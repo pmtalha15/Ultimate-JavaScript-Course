1e6a4b4134be634ff63e1d92ac4b6bcb182b6b8d
Talha <p.m.talha15@gmail.com>
2023-05-14 12:04:26 +0530

    Added folder for chapter 1 and also added practice set questions and answers

---
+++ Chapter 1/001. Practice Set/Question5.js
@@ -0,0 +1,11 @@
+// Write a Javascript program to create a word-meaning dicitonary of 5 words
+
+const words = {
+    "Hi": "greeting",
+    "Engineering": "Profession",
+    "Laptop": "Electronic Device",
+    "Android": "Software",
+    "Water": "Liquid"
+}
+
+console.log(words);
